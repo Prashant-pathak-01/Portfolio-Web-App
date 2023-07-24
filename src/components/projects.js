26952be53cdc83323516a547c4532314afcbabb1
Prashant Pathak <prashantpathak6395@gmail.com>
2023-07-24 15:06:15 +0530

    New Project added

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './projects.css';
 import MyProject from'./MyProjects/myProject';
 import PortfolioWebAppProject from '../images/portfolio_web_app_project.png'
+import GenerateQrProject from '../images/generateQr_project.png'
 function projects() {
   return (
     <div className='projects'>
       <h1>Projects Portfolio</h1>
       <div className='project_list'>
         <a href='#'><MyProject img={PortfolioWebAppProject} topic="Portfolio Web App" domain="Web Application"></MyProject></a>
+        <a href='https://generateqr-project.netlify.app/' target='_blank'><MyProject img={GenerateQrProject} topic="GenerateQr Web App" domain="Web Application"></MyProject></a>
 
       </div>
     </div>
