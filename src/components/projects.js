8e4b877035f63a2253b8dd327cdbc55eaac3dbc5
prashant-pathak-01 <prashantpathak6395@gmail.com>
2024-01-08 22:39:46 +0530

    New Projects added

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,6 +4,8 @@ import MyProject from'./MyProjects/myProject';
 import PortfolioWebAppProject from '../images/portfolio_web_app_project.png'
 import GenerateQrProject from '../images/generateQr_project.png'
 import TodoProject from '../images/todo_web_app_project.png'
+import ChatterProject from '../images/Chatter.png'
+import HospitalManagementProject from '../images/HMSS.png'
 function projects() {
   return (
     <div className='projects'>
@@ -12,6 +14,8 @@ function projects() {
         <a href='#'><MyProject img={PortfolioWebAppProject} topic="Portfolio Web App" domain="Web Application"></MyProject></a>
         <a href='https://generateqr-project.netlify.app/' target='_blank'><MyProject img={GenerateQrProject} topic="GenerateQr Web App" domain="Web Application"></MyProject></a>
         <a href='https://todo-list-webapp-project.netlify.app/' target='_blank'><MyProject img={TodoProject} topic="To do Web App" domain="Web Application"></MyProject></a>
+        <a href='https://github.com/Prashant-pathak-01/Chatter-website' target='_blank'><MyProject img={ChatterProject} topic="Chatting website" domain="Web Application"></MyProject></a>
+        <a href='https://hospital-management-system-project.netlify.app/' target='_blank'><MyProject img={HospitalManagementProject} topic="To do Web App" domain="Web Application"></MyProject></a>
 
       </div>
     </div>
