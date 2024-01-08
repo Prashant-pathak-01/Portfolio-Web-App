6448f5c4f19291c71f4b2eef91489e142055d595
prashant-pathak-01 <prashantpathak6395@gmail.com>
2024-01-08 22:40:33 +0530

    New projects added

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,7 +15,7 @@ function projects() {
         <a href='https://generateqr-project.netlify.app/' target='_blank'><MyProject img={GenerateQrProject} topic="GenerateQr Web App" domain="Web Application"></MyProject></a>
         <a href='https://todo-list-webapp-project.netlify.app/' target='_blank'><MyProject img={TodoProject} topic="To do Web App" domain="Web Application"></MyProject></a>
         <a href='https://github.com/Prashant-pathak-01/Chatter-website' target='_blank'><MyProject img={ChatterProject} topic="Chatting website" domain="Web Application"></MyProject></a>
-        <a href='https://hospital-management-system-project.netlify.app/' target='_blank'><MyProject img={HospitalManagementProject} topic="To do Web App" domain="Web Application"></MyProject></a>
+        <a href='https://hospital-management-system-project.netlify.app/' target='_blank'><MyProject img={HospitalManagementProject} topic="Hospital Management Website" domain="Web Application"></MyProject></a>
 
       </div>
     </div>
