c238425f450825a18b0f9d94e906b8213621c0d7
prashant-pathak-01 <prashantpathak6395@gmail.com>
2024-06-17 22:38:08 +0530

    Resume updated

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import './aboutMe.css';
+import React from "react";
+import "./aboutMe.css";
 function aboutMe() {
-  function resumeDownload(){
-    window.open("https://drive.google.com/file/d/1EavIo2PtY16xtFtC4qpQUpa2duq0mdMU/view?usp=sharing","_blank");
+  function resumeDownload() {
+    window.open(
+      "https://drive.google.com/file/d/1TteA_4WVZi69p_ABr4HVTjJSFvOvEfyy/view?usp=sharing",
+      "_blank"
+    );
   }
   return (
-    <div className='aboutMe'>
-        <div className='details'>
-          <h1>HI, I AM PRASHANT PATHAK</h1>
-          <h3>I am a student</h3>
-          <p>I am a student of Computer Science . i have experience in building scalable , secure , and reliable web applications using various frameworks and technologies. I enjoy solving complex Problems and Learning new skills. I am always looking for new challanges and opportunities to grow up.</p>
-          <button onClick={resumeDownload}>Download Resume</button>
-        </div>
-        <div className='programmerImg'></div>
+    <div className="aboutMe">
+      <div className="details">
+        <h1>HI, I AM PRASHANT PATHAK</h1>
+        <h3>I am a student</h3>
+        <p>
+          I am a student of Computer Science . i have experience in building
+          scalable , secure , and reliable web applications using various
+          frameworks and technologies. I enjoy solving complex Problems and
+          Learning new skills. I am always looking for new challanges and
+          opportunities to grow up.
+        </p>
+        <button onClick={resumeDownload}>Download Resume</button>
+      </div>
+      <div className="programmerImg"></div>
     </div>
-  )
+  );
 }
 
-export default aboutMe
+export default aboutMe;
